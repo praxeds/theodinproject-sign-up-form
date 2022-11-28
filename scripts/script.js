8863b55e9f096593a9ab34f1331cd0c1b833501c
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-28 11:56:54 -0300

    Added password validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const password = document.getElementById('password')
 const confirmPassword = document.getElementById('confirmPassword')
 
-confirmPassword.addEventListener('keydown', function() {
+confirmPassword.addEventListener('input', function() {
     if (confirmPassword.value !== password.value) {
         confirmPassword.style.border = '1px solid red'
         password.style.border = '1px solid red'
         document.querySelector('#confirmPassword+span').innerHTML = "passwords don't match!"
+    } else {
+        confirmPassword.style.border = '1px solid #77a265'
+        password.style.border = '1px solid #cacaca'
+        document.querySelector('#confirmPassword+span').innerHTML = ""
     }
 })
